c8c09e92a08c7b5027e4ec72bb78c5495aa43fa9
Stéphane Vanberg <vanbergstephane@gmail.com>
2021-04-05 21:55:51 +0200

    New solutions

---
+++ src/algoexpert/easy_15.java
@@ -0,0 +1,25 @@
+package algoexpert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class easy_15 {
+    public static int productSum(List<Object> array) {
+        return productSumRec(array, 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static int productSumRec(List<Object> array, int depth) {
+        int sum = 0;
+
+        for (Object object : array) {
+            if (object instanceof ArrayList) {
+                sum += productSumRec((List<Object>) object, depth + 1);
+            } else {
+                sum += (int) object;
+            }
+        }
+
+        return sum * depth;
+    }
+}
---
+++ src/algoexpert/easy_16.java
@@ -0,0 +1,42 @@
+package algoexpert;
+
+public class easy_16 {
+
+    public static int binarySearch(int[] array, int target) {
+        int start = 0;
+        int end = array.length;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            } else if (target < array[mid]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+    
+    /*
+    public static int binarySearch(int[] array, int target) {
+        return binarySearchRec(array, 0, array.length, target);
+    }
+
+    // [start; end[ 
+    public static int binarySearchRec(int[] array, int start, int end, int target) {
+        final int mid = start + ((end - start) / 2);
+
+        if (array[mid] == target) {
+            return mid;
+        } else if (target < array[mid]) {
+            return binarySearchRec(array, 0, mid, target);
+        } else {
+            return binarySearchRec(array, mid + 1, end, target);
+        }
+    }
+     */
+}
