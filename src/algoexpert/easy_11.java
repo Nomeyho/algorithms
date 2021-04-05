9e721a451748c64a095b2cda7630cd9a95dc5b25
Stéphane Vanberg <vanbergstephane@gmail.com>
2021-04-05 19:10:39 +0200

    New solutions

---
+++ src/algoexpert/easy_11.java
@@ -0,0 +1,35 @@
+package algoexpert;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class easy_11 {
+
+    public boolean classPhotos(
+            ArrayList<Integer> redShirtHeights,
+            ArrayList<Integer> blueShirtHeights
+    ) {
+        final int size = redShirtHeights.size();
+        Collections.sort(redShirtHeights);
+        Collections.sort(blueShirtHeights);
+
+        final ArrayList<Integer> backRow;
+        final ArrayList<Integer> frontRow;
+        if (redShirtHeights.get(size - 1) > blueShirtHeights.get(size - 1)) {
+            backRow = redShirtHeights;
+            frontRow = blueShirtHeights;
+        } else {
+            backRow = blueShirtHeights;
+            frontRow = redShirtHeights;
+        }
+
+        for (int i = size - 1; i >= 0; --i) {
+            if (backRow.get(i) <= frontRow.get(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
---
+++ src/algoexpert/easy_12.java
@@ -0,0 +1,27 @@
+package algoexpert;
+
+import java.util.Arrays;
+
+class easy_12 {
+
+    public int tandemBicycle(
+            int[] redShirtSpeeds,
+            int[] blueShirtSpeeds,
+            boolean fastest
+    ) {
+        final int len = redShirtSpeeds.length;
+        Arrays.sort(redShirtSpeeds);
+        Arrays.sort(blueShirtSpeeds);
+        int sum = 0;
+
+        for (int i = 0; i < len; ++i) {
+            sum += Math.max(
+                    redShirtSpeeds[i],
+                    blueShirtSpeeds[fastest ? len - i - 1 : i]
+            );
+        }
+
+        return sum;
+    }
+}
+
