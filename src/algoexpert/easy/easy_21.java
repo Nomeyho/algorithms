5758bef024ebd58d3a293d20dfef121bc202c9b9
Stéphane Vanberg <vanbergstephane@gmail.com>
2021-04-08 18:39:15 +0200

    New solutions

---
+++ src/algoexpert/easy/easy_21.java
@@ -0,0 +1,19 @@
+package algoexpert.easy;
+
+public class easy_21 {
+
+    public static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start <= end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
