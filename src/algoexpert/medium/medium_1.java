f8b711c68ffa62ffbeacd3fa5be18bc65baa288a
Stéphane Vanberg <vanbergstephane@gmail.com>
2021-04-09 22:57:35 +0200

    New solutions

---
+++ src/algoexpert/medium/medium_1.java
@@ -0,0 +1,50 @@
+package algoexpert.medium;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class medium_1 {
+    
+    // FIXME: simplier solution on AlgoExpert
+    public static void main(final String[] args) {
+        List<Integer[]> result = threeNumberSum(new int[]{8, 10, -2, 49, 14}, 57);
+        for (final Integer[] triplet : result) {
+            System.out.println(Arrays.toString(triplet));
+        }
+    }
+
+    public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+        List<Integer[]> triplets = new ArrayList<>();
+        Set<String> tripletKeys = new HashSet<>();
+        Set<Integer> set = Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.toSet());
+
+        for (int a : array) {
+            for (int b : array) {
+                int c = targetSum - a - b;
+                if (a != b && c != a && c != b) {
+                    Integer[] triplet = new Integer[] { a, b, c };
+                    Arrays.sort(triplet);
+                    String key = triplet[0] + "_" + triplet[1] + "_" + triplet[2];
+                    
+                    if (!tripletKeys.contains(key) && set.contains(targetSum - a - b)) {
+                        triplets.add(triplet);
+                        tripletKeys.add(key);
+                    }
+                }
+            }
+        }
+        
+        triplets.sort((t1, t2) -> {
+            if (!t1[0].equals(t2[0])) {
+                return t2[0] - t1[0];
+            } else if (!t1[1].equals(t2[1])) {
+                return t2[1] - t1[1];
+            } else {
+                return t2[2] - t1[2];
+            }
+        });
+        return triplets;
+    }
+}
